aa09e915289e2fe753fb09d90f581de40bc778b2
x0334 <xiaof@dtdream.com>
2020-11-03 14:33:38 +0800

    delete server directory

--- server/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-env GOOS=linux GOARCH=amd64 CGO_ENABLED=1
-
-cd /go/src/github.com/xiaof-github/kafdrop-go
-rm -f kafdrop
-go build -ldflags "-linkmode external -extldflags -static" -o kafdrop main.go
-
-echo "install finish."
