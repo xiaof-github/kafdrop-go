6f7be704c3e5dc8fba3a4e8f2037327f6fdfbf9e
x0334 <xiaof@dtdream.com>
2020-11-17 09:10:25 +0800

    get brokers

--- kafgo/kafkaUtil.go
+++ kafgo/kafkaUtil.go
@@ -3,32 +3,39 @@ package kafgo
 import (
 	"github.com/Shopify/sarama"
 	"github.com/astaxie/beego/logs"
+	"github.com/xiaof-github/kafdrop-go/models"
 )
 
 const OFFSET_INIT string = "oldest"
 
-var Brokers []*sarama.Broker
 var Client sarama.Client
 
 // get kafka client
 func GetClient(addrs []string, version string, offInit string) (sarama.Client, error) {
-	config := sarama.NewConfig()
-	ver, err := sarama.ParseKafkaVersion(version)
-	if err != nil {
-		logs.Error("Error parsing Kafka version: %v", err)
-		panic("version error")
+    config := sarama.NewConfig()
+    ver, err := sarama.ParseKafkaVersion(version)
+    if err != nil {
+        logs.Error("Error parsing Kafka version: %v", err)
+        panic("version error")
     }
-	config.Version = ver
-	config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRange
-	if offInit == OFFSET_INIT {
-		config.Consumer.Offsets.Initial = sarama.OffsetOldest
-	} else {
-		config.Consumer.Offsets.Initial = sarama.OffsetNewest
-	}
+    config.Version = ver
+    config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRange
+    if offInit == OFFSET_INIT {
+        config.Consumer.Offsets.Initial = sarama.OffsetOldest
+    } else {
+        config.Consumer.Offsets.Initial = sarama.OffsetNewest
+    }
 
-	client, err := sarama.NewClient(addrs, config)
+    client, err := sarama.NewClient(addrs, config)
     if err != nil {
         panic("kafka client create error")
-	}
-	return client, err
+    }
+    return client, err
+}
+
+// get kafka broker list
+func GetKafkaBroker() ([]models.KafkaBroker) {
+    // 获取broker节点信息
+    brokers := Client.Brokers()
+    
 }
