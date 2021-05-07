24be73bd4c7a44d8bf7ee593e264d8faec3eb3ef
xiaof <xffit@163.com>
2021-05-07 09:16:16 +0800

    str to time

--- demo/demo2021/demo_day1.go
+++ demo/demo2021/demo_day1.go
@@ -1,13 +1,32 @@
-package demo
+package main
 
 import (
 	"fmt"
 	"time"
+
+	"github.com/shirou/gopsutil/host"
 )
 
 func main() {
 
-	fmt.Println("day: ", time.Now().Local().Format("20060102"))
+	fmt.Println("day: ", time.Now().Local().Format("2006-01-02"))
+	dayBegin := time.Now().Local().Format("2006-01-02") + " 00:00:00"
+	// 此格式为固定格式
+	// Parse parses a formatted string and returns the time value it represents. The layout defines the format by showing how the reference time, defined to be Mon Jan 2 15:04:05 -0700 MST 2006 would be interpreted if it were the value; it serves as an example of the input format. The same interpretation will then be made to the input string.
+
+	format := "2006-01-02 15:04:05"
 
 	
+	timestamp, _ := host.BootTime()
+	timeN := time.Now()
+	timeNs := time.Now().Unix()
+
+	fmt.Println("boot, now, stamp, dayBegin, ", timestamp, timeN, timeNs, dayBegin)
+
+	stamp, err := time.ParseInLocation(format, dayBegin, time.Local) //使用parseInLocation将字符串格式化返回本地时区时间
+	if (err != nil ){
+		fmt.Println("err: ", err)
+	}
+	fmt.Println("time parse: ", stamp, stamp.Unix())
+
 }
