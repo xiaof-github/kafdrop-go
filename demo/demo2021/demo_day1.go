cc2f8662f26991152bd914e7850794d4e708bf3c
xiaof <xffit@163.com>
2021-05-14 08:30:46 +0800

    day print

--- demo/demo2021/demo_day1.go
+++ demo/demo2021/demo_day1.go
@@ -9,7 +9,9 @@ import (
 
 func main() {
 
-	fmt.Println("day: ", time.Now().Local().Format("2006-01-02"))
+	fmt.Println("day1: ", time.Now().Local().Format("2006-01-02"))
+	fmt.Println("day2: ", time.Now().Local().Format("20060102"))
+	
 	dayBegin := time.Now().Local().Format("2006-01-02") + " 00:00:00"
 	// 此格式为固定格式
 	// Parse parses a formatted string and returns the time value it represents. The layout defines the format by showing how the reference time, defined to be Mon Jan 2 15:04:05 -0700 MST 2006 would be interpreted if it were the value; it serves as an example of the input format. The same interpretation will then be made to the input string.
