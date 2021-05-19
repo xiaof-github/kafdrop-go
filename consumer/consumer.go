be0ed46335b7bd375681814e7169f2120a9951a1
xiaof <xffit@163.com>
2021-05-19 11:03:50 +0800

    add param num

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -1,61 +1,61 @@
 package main
 
 import (
-	"fmt"
-	"os"
+    "fmt"
+    "os"
 
-	"github.com/optiopay/kafka/v2"	
+    "github.com/optiopay/kafka/v2"	
 )
 
 func main() {
 
-	if len(os.Args) < 3 {
-		fmt.Printf("Usage: %s addr:port topic\n", os.Args[0])
-		os.Exit(1)
-	}
-	addr := os.Args[1]
-	topic := os.Args[2]
+    if len(os.Args) < 3 {
+        fmt.Printf("Usage: %s addr:port topic\n", os.Args[0])
+        os.Exit(1)
+    }
+    addr := os.Args[1]
+    topic := os.Args[2]
 
-	// connect to kafka cluster
-	// addresses := []string{"10.155.200.106:9092", "10.155.200.107:9092", "10.155.200.108:9092"}
-	addresses := []string{addr}
-	broker, err := kafka.Dial(addresses, kafka.NewBrokerConf("test"))
-	if err != nil {
-		panic(err)
-	}
-	defer broker.Close()
-	
-	resp, err := broker.Metadata()
-	for i := 0; i < len(resp.Topics); i++ {
-		println("resp topic: ", resp.Topics[i].Name)
-		tname, _ := broker.PartitionCount(resp.Topics[i].Name)
-		println("topic partition: ", tname)
-	}
+    // connect to kafka cluster
+    // addresses := []string{"10.155.200.106:9092", "10.155.200.107:9092", "10.155.200.108:9092"}
+    addresses := []string{addr}
+    broker, err := kafka.Dial(addresses, kafka.NewBrokerConf("test"))
+    if err != nil {
+        panic(err)
+    }
+    defer broker.Close()
+    
+    resp, err := broker.Metadata()
+    for i := 0; i < len(resp.Topics); i++ {
+        println("resp topic: ", resp.Topics[i].Name)
+        tname, _ := broker.PartitionCount(resp.Topics[i].Name)
+        println("topic partition: ", tname)
+    }
 
-	count, err := broker.PartitionCount(topic)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("topic: ", count, "count: ", count)
+    count, err := broker.PartitionCount(topic)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("topic: ", count, "count: ", count)
 
-	// read all messages
-	for j:=0;j<10;j++{		
-		for i:=int32(0);i<count;i++ {
-			
-			conf := kafka.NewConsumerConf(topic, i)
-			conf.StartOffset = kafka.StartOffsetNewest
-			consumer, err := broker.Consumer(conf)
-			if err != nil {
-				panic(err)
-			}
-			msg, err := consumer.Consume()
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println("Partition: ", msg.Partition, " Offset: ", msg.Offset, " Value: ", string(msg.Value))
-			
-		}					
-	}
+    // read all messages
+    for j:=0;j<10;j++{		
+        for i:=int32(0);i<count;i++ {
+            
+            conf := kafka.NewConsumerConf(topic, i)
+            conf.StartOffset = kafka.StartOffsetNewest
+            consumer, err := broker.Consumer(conf)
+            if err != nil {
+                panic(err)
+            }
+            msg, err := consumer.Consume()
+            if err != nil {
+                panic(err)
+            }
+            fmt.Println("Partition: ", msg.Partition, " Offset: ", msg.Offset, " Value: ", string(msg.Value))
+            
+        }					
+    }
 
 }
 
--- producer/sync/producerSync.go
+++ producer/sync/producerSync.go
@@ -1,74 +1,76 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-	"time"
-	"os"	
+    "fmt"    
+    "time"
+    "strconv"
+    "os"	
 
-	"github.com/Shopify/sarama"
+    "github.com/Shopify/sarama"
 )
 
 // 封装发送消息C
 func sendCMsg(msg *sarama.ProducerMessage, producer sarama.SyncProducer, msgTopic string, data string, i int) {
-	value := "{\"version\":\"V1.0.0\""
-	
-	value = value + ",\"data\":\"" + data + "\""
-	value = value + ",\"id\":\"" + strconv.Itoa(i) + "\"}"	
-	fmt.Println("msgTopic = ",msgTopic,",value = ",value)
-	msg.Topic = msgTopic
-	//将字符串转换为字节数组
-	msg.Value = sarama.ByteEncoder(value)
-	//fmt.Println(value)
-	//SendMessage：该方法是生产者生产给定的消息
-	//生产成功的时候返回该消息的分区和所在的偏移量
-	//生产失败的时候返回error
-	partition1, offset1, err1 := producer.SendMessage(msg)
+    value := data
+    fmt.Println("msgTopic = ",msgTopic,",value = ",data, "number: ", i)
+    msg.Topic = msgTopic
+    //将字符串转换为字节数组
+    msg.Value = sarama.ByteEncoder(value)
+    //fmt.Println(value)
+    //SendMessage：该方法是生产者生产给定的消息
+    //生产成功的时候返回该消息的分区和所在的偏移量
+    //生产失败的时候返回error
+    partition1, offset1, err1 := producer.SendMessage(msg)
 
-	if err1 != nil {
-		fmt.Println("Send message Fail")
-	}
-	fmt.Printf("Partition = %d, offset=%d\n", partition1, offset1)
+    if err1 != nil {
+        fmt.Println("Send message Fail")
+    }
+    fmt.Printf("Partition = %d, offset=%d\n", partition1, offset1)
 }
 
 
 
 func main() {
-	if len(os.Args) < 4 {
-		fmt.Printf("Usage: %s addr:port topic msg\n", os.Args[0])
-		os.Exit(1)
-	}
-	addr := os.Args[1]
-	topic := os.Args[2]
-	data := os.Args[3]
+    if len(os.Args) < 5 {
+        fmt.Printf("Usage: %s addr:port topic num msg\n", os.Args[0])
+        os.Exit(1)
+    }
+    addr := os.Args[1]
+    topic := os.Args[2]
+    num := os.Args[3]
+    data := os.Args[4]
 
-	config := sarama.NewConfig()
-	// 等待服务器所有副本都保存成功后的响应
-	config.Producer.RequiredAcks = sarama.WaitForLocal
-	// 随机的分区类型：返回一个分区器，该分区器每次轮询一个可用的分区
-	config.Producer.Partitioner = sarama.NewRoundRobinPartitioner
-	// 是否等待成功和失败后的响应
-	config.Producer.Return.Successes = true
+    config := sarama.NewConfig()
+    // 等待服务器所有副本都保存成功后的响应
+    config.Producer.RequiredAcks = sarama.WaitForLocal
+    // 随机的分区类型：返回一个分区器，该分区器每次轮询一个可用的分区
+    config.Producer.Partitioner = sarama.NewRoundRobinPartitioner
+    // 是否等待成功和失败后的响应
+    config.Producer.Return.Successes = true
 
-	// 使用给定代理地址和配置创建一个同步生产者
-	producer, err := sarama.NewSyncProducer([]string{addr}, config)
-	// producer, err := sarama.NewSyncProducer([]string{"10.155.200.106:9092","10.155.200.107:9092","10.155.200.108:9092"}, config)
-	if err != nil {
-		panic(err)
-	}
+    // 使用给定代理地址和配置创建一个同步生产者
+    producer, err := sarama.NewSyncProducer([]string{addr}, config)
+    // producer, err := sarama.NewSyncProducer([]string{"10.155.200.106:9092","10.155.200.107:9092","10.155.200.108:9092"}, config)
+    if err != nil {
+        panic(err)
+    }
 
-	defer producer.Close()
+    defer producer.Close()
 
-	//构建发送的消息，
-	msg := &sarama.ProducerMessage {
-		//Topic: "test",//包含了消息的主题
-		Partition: int32(20),//
-		Key:        nil,//
-	}
-	var i int = 0
-	for {		
-		sendCMsg(msg, producer, topic, data, i)
-		i++
-		time.Sleep(3*time.Second)
-	}
+    //构建发送的消息，
+    msg := &sarama.ProducerMessage {
+        //Topic: "test",//包含了消息的主题
+        Partition: int32(20),//
+        Key:        nil,//
+    }
+    var i int = 0
+    count,err := strconv.Atoi(num)
+    if err != nil {
+        panic(err)
+    }
+    for j:=0;j<count;j++{		
+        sendCMsg(msg, producer, topic, data, i)
+        i++
+        time.Sleep(3*time.Second)
+    }
 }
