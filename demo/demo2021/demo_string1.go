7b90a02d52b53521ce217c2ee8a2967afa7c3752
xiaof <xffit@163.com>
2021-06-17 08:28:26 +0800

    prestr

--- demo/demo2021/demo_string1.go
+++ demo/demo2021/demo_string1.go
@@ -12,4 +12,18 @@ func main() {
 	
 	fmt.Println(s)
 	fmt.Println(b)
+
+    t := "23333333daccccccccccccccc"
+
+    fmt.Println(PreStr(t, 10))
+}
+
+func PreStr(str string, length int) string {
+	var i int
+	for i = range str {
+		if length == i{
+			break
+		}		
+	}
+	return str[:i]
 }
