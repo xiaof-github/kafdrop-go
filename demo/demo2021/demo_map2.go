1dd4dd6351676401c00a7e70d422c15cf8b0bc3d
xiaof <xffit@163.com>
2021-08-23 19:32:11 +0800

    map

--- demo/demo2021/demo_map2.go
+++ demo/demo2021/demo_map2.go
@@ -5,11 +5,28 @@ import (
 	"strconv"
 )
 func main() {
-	x := make(map[int]string, 20)
+	x := make(map[string]int, 20)
 	for i := 0; i < 20; i++ {
-		x[i] = strconv.Itoa(i)
+		x[strconv.Itoa(i)] = i 
 	}
-    delete(x, 1)
-    delete(x, 0)
     fmt.Println(x)
+    conf := []string{"1","2","3"}
+    fmt.Println(conf)
+    cutProcess(x, conf)
+    fmt.Println("after cut", x)
+}
+
+func cutProcess(runTimeMap map[string]int, processConf []string) {
+    proMap := make(map[string]int, len(processConf))
+
+    for _, v := range processConf {
+        proMap[v] = 0
+    }
+
+    for k, _ := range runTimeMap {
+        _, ok := proMap[k]
+        if !ok {
+            delete(runTimeMap, k)
+        }
+    }
 }
